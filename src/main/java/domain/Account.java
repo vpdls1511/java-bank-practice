b9958d8012026802330fe258d9687ecf270184f9
nGyu <vpdls1511@gmail.com>
2026-07-14 22:26:00 +0900

    feat : Account 도메인 작성

---
+++ src/main/java/domain/Account.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.math.BigDecimal;
+
+public class Account {
+  private final String name;
+  private String accountNumber;
+  private BigDecimal money;
+
+  public Account(String name) {
+    this.name = name;
+    this.accountNumber = generateAccountNumber();
+    this.money = BigDecimal.ZERO;
+  }
+
+  private String generateAccountNumber() {
+    return String.valueOf(System.currentTimeMillis() % 1000000);
+  }
+}
