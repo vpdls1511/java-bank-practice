9ee6499a9a2d6a754e648cf31a76eb35aa671ef9
nGyu <vpdls1511@gmail.com>
2026-07-14 22:33:37 +0900

    feat : account 목록을 저장하기 위한 Accounts 생성

---
+++ src/main/java/domain/Accounts.java
@@ -0,0 +1,33 @@
+package domain;
+
+public class Accounts {
+  private Account[] accounts = {};
+
+  public Accounts() {}
+
+  public void add(Account account) {
+    Account[] newAccounts = new Account[accounts.length + 1];
+
+    for (int i = 0 ; i <= accounts.length ; i++) {
+      newAccounts[i] = accounts[i];
+    }
+
+    newAccounts[newAccounts.length - 1] = account;
+
+    accounts = newAccounts;
+  }
+
+  public Account get(int i) {
+    return accounts[i];
+  }
+
+  public Account get(String accountNumber) {
+    for(Account account: accounts) {
+      if (accountNumber.equals(account.getAccountNumber())) {
+        return account;
+      }
+    }
+
+    return null;
+  }
+}
