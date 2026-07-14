19d62d011e9afc916ffd9c341286ecd1c89afa8b
nGyu <vpdls1511@gmail.com>
2026-07-14 20:21:47 +0900

    feat : MenuItem 렌더링

--- src/main/java/ui/Menu.java
+++ src/main/java/ui/Menu.java
@@ -3,11 +3,11 @@ package ui;
 public class Menu {
 
   public static void render() {
-    System.out.println("========================\n"
-                               + "01. 계좌 생성\n"
-                               + "02. 예금 입금\n"
-                               + "03. 예금 출금\n"
-                               + "04. 계좌 목록 조회\n"
-                               + "========================");
+    System.out.println("=================");
+    for (MenuItem item : MenuItem.values()) {
+      System.out.printf("%s. %s \n",
+                        item.getCode(), item.getLabel());
+    }
+    System.out.println("=================");
   }
 }
