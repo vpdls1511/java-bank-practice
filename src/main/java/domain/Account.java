95325b1c77a9d88870cca68b9dbbd7e54cc69fe1
nGyu <vpdls1511@gmail.com>
2026-07-15 21:49:48 +0900

    feat : 입/출금 가능

--- src/main/java/domain/Account.java
+++ src/main/java/domain/Account.java
@@ -25,6 +25,35 @@ public class Account {
     return this.money;
   }
 
+  /**
+   * 입금
+   */
+  public void deposit(long money) {
+    deposit(BigDecimal.valueOf(money));
+  }
+
+  public void deposit(String money) {
+    deposit(new BigDecimal(money));
+  }
+
+  public void deposit(BigDecimal money) {
+    this.money = this.money.add(money);
+  }
+
+  /**
+   * 출금
+   */
+  public void withdrawn(long money) {
+    this.withdrawn(BigDecimal.valueOf(money));
+  }
+
+  public void withdrawn(BigDecimal money) {
+    if (this.money.compareTo(money) < 0) {
+      throw new ArithmeticException("출금 한도가 부족합니다.");
+    }
+    this.money = this.money.subtract(money);
+  }
+
   @Override
   public String toString() {
     return "이름 : " + this.name
