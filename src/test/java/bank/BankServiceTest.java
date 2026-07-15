c749de90f537910de3ab79079d6456c41781c45f
nGyu <vpdls1511@gmail.com>
2026-07-15 22:10:41 +0900

    test : 입금 테스트

---
+++ src/test/java/bank/BankServiceTest.java
@@ -0,0 +1,64 @@
+package bank;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import domain.Account;
+import domain.Accounts;
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.math.BigDecimal;
+import java.rmi.NoSuchObjectException;
+import org.junit.jupiter.api.Test;
+
+class BankServiceTest {
+  private final Account account = new Account("그린");
+
+  private final BankService bankService = new BankService();
+
+  @Test
+  void noSuchAccounts() {
+    assertThrows(NoSuchObjectException.class,
+                 () -> bankService.deposit(account.getAccountNumber(), 1000L));
+  }
+
+  @Test
+  void noSuchAccountNumber() {
+    assertThrows(NoSuchObjectException.class,
+                 () -> bankService.deposit("10a0d0", 1000L));
+  }
+
+  @Test
+  void canDepositMoney() {
+    long money = 10000L;
+
+    Account myAccount = bankService.createAccount("Green");
+    bankService.deposit(myAccount.getAccountNumber(), money);
+
+    assertEquals(BigDecimal.valueOf(money), myAccount.getMoney());
+  }
+
+  @Test
+  void canWithdrawnMoney() {
+    long money = 10000L;
+    Account myAccount = bankService.createAccount("Green");
+
+    bankService.deposit(myAccount.getAccountNumber(), money);
+    myAccount.deposit(money);
+    myAccount.withdrawn(money);
+
+    assertEquals(BigDecimal.ZERO, account.getMoney());
+  }
+
+  @Test
+  void canWithdrawnMoneyByArithmeticException() {
+    long money = 10000L;
+    Account myAccount = bankService.createAccount("Green");
+
+    bankService.deposit(account.getAccountNumber(), money);
+    myAccount.deposit(money);
+    myAccount.withdrawn(money);
+
+    assertEquals(BigDecimal.ZERO, account.getMoney());
+  }
+}
