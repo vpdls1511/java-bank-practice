24640f40a4ffcd1c80863682fa706bbde09b286f
nGyu <vpdls1511@gmail.com>
2026-07-17 23:01:33 +0900

    feat : 종료 문구 수정

--- src/main/java/utils/ui/Log.java
+++ src/main/java/utils/ui/Log.java
@@ -3,7 +3,7 @@ package utils.ui;
 public class Log {
 
   public static void stopProcess() {
-    System.out.print("\n다시 시도해주세요\n");
+    System.out.print("\n재시도 횟수를 모두 사용했습니다. 처음부터 다시 진행해 주세요.\n");
   }
 
   public static void retry(Exception e) {
